fc99c3432db24999c82a84a06074026c2bfe6663
timkeller2 <tim@kellpro.com>
2026-06-06 10:35:10 -0500

    Fix Lyran and Klingon cruiser/capital ship designs

--- src/main.js
+++ src/main.js
@@ -6905,10 +6905,136 @@ window.addEventListener('keyup', e => keysDown[e.key] = false);
               ctx.lineTo(size * 0.2, 0);
             }
           } else if (style === 'Lyran') {
-            ctx.lineTo(-size * 0.8, -size * 0.1);
-            ctx.lineTo(-size / 10, -size * 0.2);
-            ctx.lineTo(-size / 8, -size * 0.75);
-            ctx.lineTo(-size / 6, -size * 0.85);
+            if (cohort === 'cruiser_group') {
+              ctx.moveTo(0, -size);
+              ctx.lineTo(size * 0.4, -size * 0.8);
+              ctx.lineTo(size * 0.4, -size * 0.4);
+              ctx.lineTo(size * 0.8, -size * 0.2);
+              ctx.lineTo(size * 0.9, size * 0.3);
+              ctx.lineTo(size * 0.5, size * 0.1);
+              ctx.lineTo(size * 0.4, size * 0.7);
+              ctx.lineTo(0, size * 0.5);
+              ctx.lineTo(-size * 0.4, size * 0.7);
+              ctx.lineTo(-size * 0.5, size * 0.1);
+              ctx.lineTo(-size * 0.9, size * 0.3);
+              ctx.lineTo(-size * 0.8, -size * 0.2);
+              ctx.lineTo(-size * 0.4, -size * 0.4);
+              ctx.lineTo(-size * 0.4, -size * 0.8);
+            } else if (cohort === 'battleship_group') {
+              ctx.moveTo(0, -size);
+              ctx.lineTo(size * 0.5, -size * 0.7);
+              ctx.lineTo(size * 0.5, -size * 0.3);
+              ctx.lineTo(size * 1.0, -size * 0.1);
+              ctx.lineTo(size * 1.1, size * 0.4);
+              ctx.lineTo(size * 0.6, size * 0.2);
+              ctx.lineTo(size * 0.6, size * 0.8);
+              ctx.lineTo(size * 0.3, size * 0.9);
+              ctx.lineTo(0, size * 0.7);
+              ctx.lineTo(-size * 0.3, size * 0.9);
+              ctx.lineTo(-size * 0.6, size * 0.8);
+              ctx.lineTo(-size * 0.6, size * 0.2);
+              ctx.lineTo(-size * 1.1, size * 0.4);
+              ctx.lineTo(-size * 1.0, -size * 0.1);
+              ctx.lineTo(-size * 0.5, -size * 0.3);
+              ctx.lineTo(-size * 0.5, -size * 0.7);
+            } else if (cohort === 'mammoth_group') {
+              ctx.moveTo(-size * 0.3, -size * 0.9);
+              ctx.lineTo(size * 0.3, -size * 0.9);
+              ctx.lineTo(size * 0.4, -size * 0.4);
+              ctx.lineTo(size * 1.2, -size * 0.4);
+              ctx.lineTo(size * 1.2, size * 0.6);
+              ctx.lineTo(size * 0.4, size * 0.6);
+              ctx.lineTo(size * 0.3, size * 0.9);
+              ctx.lineTo(-size * 0.3, size * 0.9);
+              ctx.lineTo(-size * 0.4, size * 0.6);
+              ctx.lineTo(-size * 1.2, size * 0.6);
+              ctx.lineTo(-size * 1.2, -size * 0.4);
+              ctx.lineTo(-size * 0.4, -size * 0.4);
+            } else {
+              ctx.moveTo(-size * 0.7, -size * 0.9);
+              ctx.lineTo(-size * 0.3, -size * 0.9);
+              ctx.lineTo(-size * 0.3, size * 0.2);
+              ctx.lineTo(size * 0.3, size * 0.2);
+              ctx.lineTo(size * 0.3, -size * 0.9);
+              ctx.lineTo(size * 0.7, -size * 0.9);
+              ctx.lineTo(size * 0.9, size * 0.6);
+              ctx.lineTo(size * 0.4, size * 0.6);
+              ctx.lineTo(size * 0.2, size * 0.9);
+              ctx.lineTo(-size * 0.2, size * 0.9);
+              ctx.lineTo(-size * 0.4, size * 0.6);
+              ctx.lineTo(-size * 0.9, size * 0.6);
+            }
+          } else {
+            if (cohort === 'cruiser_group') {
+              ctx.moveTo(0, -size);
+              ctx.lineTo(size * 0.15, -size * 0.8);
+              ctx.lineTo(size * 0.1, -size * 0.3);
+              ctx.lineTo(size * 0.7, -size * 0.3);
+              ctx.lineTo(size * 0.9, size * 0.2);
+              ctx.lineTo(size * 0.8, size * 0.5);
+              ctx.lineTo(size * 0.4, size * 0.3);
+              ctx.lineTo(size * 0.2, size * 0.6);
+              ctx.lineTo(0, size * 0.7);
+              ctx.lineTo(-size * 0.2, size * 0.6);
+              ctx.lineTo(-size * 0.4, size * 0.3);
+              ctx.lineTo(-size * 0.8, size * 0.5);
+              ctx.lineTo(-size * 0.9, size * 0.2);
+              ctx.lineTo(-size * 0.7, -size * 0.3);
+              ctx.lineTo(-size * 0.1, -size * 0.3);
+              ctx.lineTo(-size * 0.15, -size * 0.8);
+            } else if (cohort === 'battleship_group') {
+              ctx.moveTo(0, -size);
+              ctx.lineTo(size * 0.25, -size * 0.9);
+              ctx.lineTo(size * 0.25, -size * 0.4);
+              ctx.lineTo(size * 0.8, -size * 0.4);
+              ctx.lineTo(size * 1.1, size * 0.2);
+              ctx.lineTo(size * 1.0, size * 0.6);
+              ctx.lineTo(size * 0.5, size * 0.3);
+              ctx.lineTo(size * 0.3, size * 0.7);
+              ctx.lineTo(0, size * 0.8);
+              ctx.lineTo(-size * 0.3, size * 0.7);
+              ctx.lineTo(-size * 0.5, size * 0.3);
+              ctx.lineTo(-size * 1.0, size * 0.6);
+              ctx.lineTo(-size * 1.1, size * 0.2);
+              ctx.lineTo(-size * 0.8, -size * 0.4);
+              ctx.lineTo(-size * 0.25, -size * 0.4);
+              ctx.lineTo(-size * 0.25, -size * 0.9);
+            } else if (cohort === 'mammoth_group') {
+              ctx.moveTo(-size * 0.3, -size * 0.9);
+              ctx.lineTo(size * 0.3, -size * 0.9);
+              ctx.lineTo(size * 0.3, -size * 0.5);
+              ctx.lineTo(size * 1.2, -size * 0.5);
+              ctx.lineTo(size * 1.3, size * 0.5);
+              ctx.lineTo(size * 0.5, size * 0.5);
+              ctx.lineTo(size * 0.4, size * 0.9);
+              ctx.lineTo(-size * 0.4, size * 0.9);
+              ctx.lineTo(-size * 0.5, size * 0.5);
+              ctx.lineTo(-size * 1.3, size * 0.5);
+              ctx.lineTo(-size * 1.2, -size * 0.5);
+              ctx.lineTo(-size * 0.3, -size * 0.5);
+            } else {
+              ctx.moveTo(0, -size);
+              ctx.lineTo(size / 6, -size * 0.85);
+              ctx.lineTo(size / 8, -size * 0.75);
+              ctx.lineTo(size / 10, -size * 0.2);
+              ctx.lineTo(size * 0.8, -size * 0.1);
+              ctx.lineTo(size * 0.9, size * 0.2);
+              ctx.lineTo(size * 0.9, size * 0.5);
+              ctx.lineTo(size * 0.7, size * 0.5);
+              ctx.lineTo(size * 0.6, size * 0.3);
+              ctx.lineTo(size / 4, size * 0.4);
+              ctx.lineTo(size / 5, size * 0.6);
+              ctx.lineTo(-size / 5, size * 0.6);
+              ctx.lineTo(-size / 4, size * 0.4);
+              ctx.lineTo(-size * 0.6, size * 0.3);
+              ctx.lineTo(-size * 0.7, size * 0.5);
+              ctx.lineTo(-size * 0.9, size * 0.5);
+              ctx.lineTo(-size * 0.9, size * 0.2);
+              ctx.lineTo(-size * 0.8, -size * 0.1);
+              ctx.lineTo(-size / 10, -size * 0.2);
+              ctx.lineTo(-size / 8, -size * 0.75);
+              ctx.lineTo(-size / 6, -size * 0.85);
+            }
           }
           
           ctx.closePath();
