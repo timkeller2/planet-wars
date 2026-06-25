0a31bcc1d68690738e5e6611963f7a8f1952d1fd
timkeller2 <tim@kellpro.com>
2026-06-25 09:26:51 -0500

    Configure neutral planets to regain supplies at 1/4 rate

--- src/entities/Planet.js
+++ src/entities/Planet.js
@@ -163,7 +163,8 @@ export class Planet {
       if (this.supplies === undefined) {
         this.supplies = Math.random() * this.maxShips;
       }
-      const regenRatePerMs = (3 * (this.maxShips / 100)) / 60000;
+      const baseRate = this.owner ? 3 : 0.75;
+      const regenRatePerMs = (baseRate * (this.maxShips / 100)) / 60000;
       this.supplies = Math.min(this.maxShips, this.supplies + regenRatePerMs * deltaTime);
     }
     this.prorateSympathiesIfNeeded();
